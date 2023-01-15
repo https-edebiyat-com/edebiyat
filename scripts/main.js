b11f85bb976ac1388144cf03900780e59b8459c0
ömer <allahdiyenayatokirishima@gmail.com>
2023-01-16 01:12:05 +0300

    first commit
    hi.

---
+++ scripts/main.js
@@ -0,0 +1,322 @@
+//#region Question 1
+var q1 = "Aşağıdaki Tanzimat dönemi sanatçılarından hangisi ilk tiyatro, ilk özel gazete, batılı anlamda ilk fabl gibi ilklere imza atmıştır?";
+
+var q1_a = "Namık Kemal";
+var q1_b = "Şemsettin Sami";
+var q1_c = "Şinasi";
+
+var q1_a_content = "Namık Kemal, Türk milliyetçiliğine ilham kaynağı olmuş, Genç Osmanlı hareketi mensubu yazar, gazeteci, devlet adamı ve şairdir. Yurtseverliğe bağlı bir Tanzimat Devri aydınıdır.";
+var q1_b_content = "Şemseddin Sami Bey; Arnavut yazar , filozof , oyun yazarı ve iki kardeşi Abdyl ile Arnavutluk Ulusal Rönesans hareketi Rilindja Kombëtare'nin önde gelen isimlerinden biriydi."
+var q1_c_content = "İbrahim Şinasi, Türk gazeteci, yayımcı, şair ve oyun yazarı. Türk toplumunda Tanzimat’ın ilanı ile başlayan batılılaşma sürecinin ilk ve en önemli yazarlarındandır."
+
+//c = true;
+
+//#endregion
+
+//#region Question 2
+
+var q2 = "Aşağıdakilerden hangisi Tevfik Fikret’in akıl hocası olan Recaizade Mahmut Ekrem’in ilk realist romanımız olan eseridir?";
+
+var q2_a = "Araba Sevdası";
+var q2_b = "Felâtun Bey ile Râkım Efendi";
+var q2_c = "Tutunamayanlar";
+
+var q2_a_content = "Araba Sevdası, 1896 yılında Servet-i Fünûn mecmuasında resimli olarak tefrika edilen sonra 1897’de kitap halinde yayımlanan yılında yayımlanan romandır.";
+var q2_b_content = "Felâtun Bey'le Râkım Efendi, 1875 yılında yazılmış romandır. Tanzimat'ı takiben ortaya çıkan ilk Türk romanlarının ana teması 'Yanlış Batılılaşma' üzerine kurulmuştur."
+var q2_c_content = "1970 yılında TRT Roman Ödülü'nü kazanan romandır. 2002 yılında UNESCO, Tutunamayanlar'ı İngilizce'ye tercüme edilmesi gereken seçkin edebiyat eseri listesine seçti."
+
+//a = true;
+
+//#endregion
+
+//#region Question 3
+var q3 = "Aşağıdaki sanatçılardan hangisi sanat anlayışı bakımından diğerlerinden farklıdır?";
+
+var q3_a = "Nabizade Nazım";
+var q3_b = "Namık Kemal";
+var q3_c = "Samipaşazade Sezai";
+
+var q3_a_content = "Şiir, anı, hikâye, roman türlerinde ve bilimsel konularda eserler veren Nabizade Nazım, ilk Türkçe gerçekçi köy romanı olarak kabul edilen Karabibik'in yazarıdır.";
+var q3_b_content = "Namık Kemal, Türk milliyetçiliğine ilham kaynağı olmuş, Genç Osmanlı hareketi mensubu yazar, gazeteci, devlet adamı ve şairdir. Yurtseverliğe bağlı bir Tanzimat Devri aydınıdır."
+var q3_c_content = "Samipaşazade Sezai, Türk edebiyatında önde gelen ışıklarından biri olan Türk eğitimci, siyasetçi, diplomat ve yazardı."
+
+//b = true;
+
+//#endregion
+
+//#region Question 4
+var q4 = "Aşağıdaki Şairlerden hangisi “Vatan Mersiyesi” eserini yazmıştır?";
+
+var q4_a = "Şinasi";
+var q4_b = "Tevfik Fikret";
+var q4_c = "Namık Kemal";
+
+var q4_a_content = "İbrahim Şinasi, Türk gazeteci, yayımcı, şair ve oyun yazarı. Türk toplumunda Tanzimat’ın ilanı ile başlayan batılılaşma sürecinin ilk ve en önemli yazarlarındandır.";
+var q4_b_content = "Şair ve öğretmen. Osmanlı İmparatorluğu'nun dağılma sürecinde Servet-i Fünûn topluluğunun lideri olan Tevfik Fikret, devrimci fikirleriyle Mustafa Kemal başta olmak üzere dönemin pek çok aydınını etkiledi."
+var q4_c_content = "Namık Kemal, Türk milliyetçiliğine ilham kaynağı olmuş, Genç Osmanlı hareketi mensubu yazar, gazeteci, devlet adamı ve şairdir. Yurtseverliğe bağlı bir Tanzimat Devri aydınıdır."
+
+//c = true;
+
+//#endregion
+
+//#region Question 5
+var q5 = "'Sanat şahsi ve muhteremdir.' sözü hangi edebi gruba aittir?";
+
+var q5_a = "Fecr-i Âti";
+var q5_b = "Beş Hececiler";
+var q5_c = "Yedi Meşaleciler";
+
+var q5_a_content = "Fecr-i Ati bir edebi topluluktur. Fecr-i Ati batıdaki benzerlerinde olduğu gibi belli ilkeler çevresinde birleşen bir yazın topluluğu biçiminde ortaya çıkmıştır.";
+var q5_b_content = "I. Meşrutiyet’ten sonra hece vezniyle ve konuşulan halk diliyle, Millî Edebiyat akımının görüşleri doğrultusunda şiir yazan beş şairin Türk edebiyatındaki genel adıdır."
+var q5_c_content = "Beş Hececiler'e tepki olarak ortaya çıkan Cumhuriyet dönemi edebî topluluğu. 1928 yılında yayımladıkları Yedi Meşale adlı dergi ile adlarını duyuran altı şair ve bir öykü yazarından oluştular."
+
+//a = true;
+
+//#endregion
+
+
+//#region Question 6
+var q6 = " Aşağıdakilerden hangisi, Fecr-i Âti topluluğunda yer almış sanatçılardan biri değildir?";
+
+var q6_a = "Ali Canip";
+var q6_b = "Emin Bülent";
+var q6_c = "Yahya Kemal";
+
+var q6_a_content = "Ali Canip Yöntem; Türk şair, yazar, edebiyat tarihi araştırmacısı ve siyasetçidir. Millî Edebiyat akımının teorisyenleri arasında yer almıştır. TBMM 4, 5, 6 ve 11. Dönem Milletvekilliği yapmıştır.";
+var q6_b_content = "Emin Bülent Serdaroğlu, Galatasaray Lisesi mezunu eski Türk futbolcu ve şairdir. Galatasaray’ın ilk Türk kaptanıdır. Victor Hugo’nun “Mavi Gözlü Yunan” şiirine karşılık yazdığı “Kin”(1910) şiiri meşhurdur."
+var q6_c_content = "Cumhuriyet dönemi Türk şiirinin temsilcilerindendir. Şiirleri Divan edebiyatı ile modern şiir arasında köprülük görevi üstlenmiştir. Türk edebiyat tarihinde Dört Aruzcu'dan biri olarak kabul edilir."
+
+//c = true;
+
+//#endregion
+
+//#region Question 7
+
+var q7 = "Aşağıdakilerden hangisi Milli Edebiyat Dönemi sanatçılarından biri değildir?";
+
+var q7_a = "Halide Edip Adıvar";
+var q7_b = "Harun Yılmaz";
+var q7_c = "Yakup Kadri Karaosmanoğlu";
+
+var q7_a_content = "Halide Onbaşı olarak da bilinir. Yazdığı yirmi bir roman, dört hikâye kitabı, iki tiyatro eseri ve çeşitli incelemeleriyle Meşrutiyet dönemi Türk edebiyatının en çok eser veren yazarlarındandır. ";
+var q7_b_content = "Seni buraya koyduğum için özür dilerim abi."
+var q7_c_content = "Roman, öykü ve makaleleri ile Türk toplumunun Tanzimat’tan bu yana geçirdiği değişiklikleri anlatmış bir yazardır. Millî Mücadeleden itibaren Atatürk’ün yakın arkadaşları arasında yer almıştır."
+
+//a = true;
+
+//#endregion
+
+//#region Question 8
+var q8 = "'Demdeme' kimin eseridir?";
+
+var q8_a = "Recaizade Mahmut Ekrem";
+var q8_b = "Muallim Naci";
+var q8_c = "Abdülhak Hamit Tarhan";
+
+var q8_a_content = "Eski edebiyatı savunan Muallim Naci ve çevresiyle girdiği edebiyat tartışmalarıyla Edebiyat-ı Cedide akımının doğmasına zemin hazırladı. Tanzimat ve Batı düşüncesini yeni kuşağa aktardı.";
+var q8_b_content = "Aruz veznini Türkçeye kusursuzca uygulamak için çalışmış bir Tanzimat dönemi şairidir. Eski ile bağları koparmadan yenileşmeyi savunmuş; edebiyat tarihinde “eski şiir”in temsilcisi sayılmıştır."
+var q8_c_content = "Osmanlı İmparatorluğu'nun son döneminde ve Cumhuriyet Türkiye'sinin ilk yıllarında eserler vermiş, modern edebiyatın doğuşunda etkin bir isimdir."
+
+//b = true;
+
+//#endregion
+
+//#region Question 9
+var q9 = "Aşağıdakilerden hangisi saray baskısı nedeniyle toplumsal eserlerin verilmesinin zorlaştığı dönemdir?";
+
+var q9_a = "II. Tanzimat Dönemi";
+var q9_b = "I. Tanzimat Dönemi";
+var q9_c = "Lale Devri";
+
+var q9_a_content = "Bu dönem edebiyatı 1. Meşrutiyetin iptal edildiği tarih olan 1878 ile 1895 yılları arasını kapsar. Dil oldukça ağırlaştırılmıştır, realizm ve natüralizm baskın akımlar olarak göze çarpar.";
+var q9_b_content = "1860 ile 1877 yılları arasını kapsayan dönemdir. Gazetenin çıkarılmasıyla birlikte roman, hikaye, tiyatro, deneme, makale gibi birçok türün ilk örnekleri verilmiştir.";
+var q9_c_content = "1718 yılında Pasarofça anlaşmasıyla başlayan Lale devri, Patrona Halil İsyanı ile sona ermiştir. Batı ile olan ilişkiler gelişmiş ve reform hareketleri başlamıştır."
+
+//c = true;
+
+//#endregion
+
+//#region Question 10
+var q10 = "Aşağıdakilerden hangisi Osmanlı Devleti'ndeki ilk resmi gazetedir?";
+
+var q10_a = "Cerîde-i Havâdis";
+var q10_b = "Tercümân-ı Ahvâl";
+var q10_c = "Takvim-i Vekayi";
+
+var q10_a_content = "Osmanlı'da William Churchill tarafından 1840'ta çıkartılmıştır. Bilimsel içeriğiyle dikkat çekse de asıl çıkış amacı; İngilizlerin çıkarlarına yönelik bir kamuoyu oluşturmaktı.";
+var q10_b_content = "Agah Efendi ve Şinasi tarafından çıkarıldı. Önceleri haftada üç gün yayımlanan gazete, zamanla Ceride-i Havadis gazetesiyle rekabet edebilmek için yayınını beş güne çıkardı."
+var q10_c_content = "Takvim-i Vekayi Dergisi yılda iki kere Türkçe ve İngilizce dillerinde yayın yapan uluslararası hakemli bilimsel bir dergidir. Yazarlardan makalelerin yayımlanması için herhangi bir ücret talep edilmemektedir. "
+
+//a = true;
+
+//#endregion
+
+//#region Arrays
+
+var questions = new Array(q1, q2, q3, q4, q5, q6, q7, q8, q9, q10);
+
+var keys = new Array("c", "a", "b", "c", "a", "c", "b", "b", "a", "c");
+
+var answers = [
+    [q1_a, q1_b, q1_c],
+    [q2_a, q2_b, q2_c],
+    [q3_a, q3_b, q3_c],
+    [q4_a, q4_b, q4_c],
+    [q5_a, q5_b, q5_c],
+    [q6_a, q6_b, q6_c],
+    [q7_a, q7_b, q7_c],
+    [q8_a, q8_b, q8_c],
+    [q9_a, q9_b, q9_c],
+    [q10_a, q10_b, q10_c],
+];
+
+var contents = [
+    [q1_a_content, q1_b_content, q1_c_content],
+    [q2_a_content, q2_b_content, q2_c_content],
+    [q3_a_content, q3_b_content, q3_c_content],
+    [q4_a_content, q4_b_content, q4_c_content],
+    [q5_a_content, q5_b_content, q5_c_content],
+    [q6_a_content, q6_b_content, q6_c_content],
+    [q7_a_content, q7_b_content, q7_c_content],
+    [q8_a_content, q8_b_content, q8_c_content],
+    [q9_a_content, q9_b_content, q9_c_content],
+    [q10_a_content, q10_b_content, q10_c_content]
+];
+//#endregion
+
+//when site is opened
+window.onload = function () {
+    dataChange();
+};
+
+//variable of sayacxD
+var i = 0;
+
+// change the question
+function dataChange() {
+
+    if (i != 10) {
+        document.getElementById('question').innerHTML = questions[i];
+
+        document.getElementById('answer-a').innerHTML = answers[i][0];
+        document.getElementById('answer-b').innerHTML = answers[i][1];
+        document.getElementById('answer-c').innerHTML = answers[i][2];
+
+        document.getElementById('answer-a-content').innerHTML = contents[i][0];
+        document.getElementById('answer-b-content').innerHTML = contents[i][1];
+        document.getElementById('answer-c-content').innerHTML = contents[i][2];
+
+        i = i + 1;
+    }
+
+}
+
+//#region Element Variables & Button Click Events
+
+const a_sure = document.getElementById("a");
+const b_sure = document.getElementById("b");
+const c_sure = document.getElementById("c");
+
+a_sure.addEventListener("click", aClick);
+b_sure.addEventListener("click", bClick);
+c_sure.addEventListener("click", cClick);
+
+//#endregion
+
+function change_color(color) {
+
+    var css = '.container .card:hover .face.face1{ background: ' + color + '; transform: translateY(0); background-size: 400% 400%; animation: gradient 1s ease infinite; }';
+    var style = document.createElement('style');
+
+    if (style.styleSheet) {
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
+
+    document.getElementsByTagName('head')[0].appendChild(style);
+}
+
+function show_true(true_answer) {
+
+    var css = '#face-' + true_answer + ':hover{ background: green; }';
+    var style = document.createElement('style');
+
+    if (style.styleSheet) {
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
+
+    document.getElementsByTagName('head')[0].appendChild(style);
+}
+//animations of translation
+function translation(status, color_name) {
+
+    //true or false
+    true_status = status;
+
+    //green or red
+    change_color(color_name)
+
+    if (true_status == true) {
+        var audio = new Audio('../sounds/victory.mp3');
+        audio.play();
+    }
+    else {
+        var audio = new Audio('../sounds/lose.mp3');
+        audio.play();
+    }
+
+    //wait 1 sec.
+    setTimeout(function aFun() {
+
+        //change question
+        dataChange();
+
+        change_color('#00367e');
+
+        //enter default status
+        true_status = false;
+    },
+        3000);
+}
+
+//default true status
+var true_status = false;
+
+//when i click a
+function aClick() {
+    if (true_status != true) {
+        if (i == 2 || i == 5 || i == 9) {
+            translation(true, "linear-gradient(90deg, #fdcb6e 0%, #a29bfe 20%, #0984e3 40%, #e84393 60%, #d63031 80%, green 100%)");
+        }
+        else {
+            translation(false, "red");
+        }
+    }
+}
+
+//when i click b
+function bClick() {
+    if (true_status != true) {
+        if (i == 3 || i == 7 || i == 8) {
+            translation(true, "linear-gradient(90deg, #fdcb6e 0%, #a29bfe 20%, #0984e3 40%, #e84393 60%, #d63031 80%, green 100%)");
+        }
+        else {
+            translation(false, "red");
+        }
+    }
+}
+
+//when i click c
+function cClick() {
+    if (true_status != true) {
+        if (i == 1 || i == 4 || i == 6 || i == 10) {
+            translation(true, "linear-gradient(90deg, #fdcb6e 0%, #a29bfe 20%, #0984e3 40%, #e84393 60%, #d63031 80%, green 100%)");
+        }
+        else {
+            translation(false, "red");
+        }
+    }
+}
